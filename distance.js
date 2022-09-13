26e1568b9ef95693e907e54a2e5850bc139cbfdb
couchmeka <meka.couch2@gmail.com>
2022-09-13 14:31:36 -0400

    distance

--- distance.js
+++ distance.js
@@ -3,8 +3,8 @@ let x2 = 15;
 let y1 = 20;
 let y2 = 25;
 
-let xFormula = (x2 - x1);
-let yFormula = (y2 - y1);
+let xFormula = x2 - x1;
+let yFormula = y2 - y1;
 
 xFormula = xFormula ** 2;
 yFormula = yFormula ** 2;
