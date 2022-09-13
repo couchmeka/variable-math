2478ec487f4243cab6f9c2d753cdd67f32693899
couchmeka <meka.couch2@gmail.com>
2022-09-13 14:54:12 -0400

    distance

--- distance.js
+++ distance.js
@@ -1,7 +1,7 @@
-let x1 = 5;
+let x1 = 20;
 let x2 = 15;
-let y1 = 20;
-let y2 = 25;
+let y1 = 30;
+let y2 = 5;
 
 let xFormula = x2 - x1;
 let yFormula = y2 - y1;
@@ -11,5 +11,7 @@ yFormula = yFormula ** 2;
 
 distance = yFormula + xFormula;
 
+distance = Math.sqrt(distance)
 
-console.log(`The distance between (${x1},${y1}) and (${x2},${y2}) is ${Math.sqrt ,distance}`);
+
+console.log(`The distance between (${x1},${y1}) and (${x2},${y2}) is ${distance}`);
