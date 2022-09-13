4a83b37068b016636c153bcbe323e9c72063fa36
couchmeka <meka.couch2@gmail.com>
2022-09-13 14:07:36 -0400

    remainder

--- remainder.js
+++ remainder.js
@@ -1,4 +1,4 @@
 let number1 = 20;
 let number2 = 40;
-
-console.log(number1 % number2);
+let remainder = number1 % number2;
+console.log(`the remainder of ${number1} divided by ${number2} is ${remainder}`);
